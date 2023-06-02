3c90f9c230c592406e2592770a56aaaeedf291f5
o22009592 <leo.oger@etu.unv-amu.fr>
2023-06-02 15:19:06 +0200

    Ajout du premier point

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -17,6 +17,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -30,6 +31,26 @@ public class ToileController implements Initializable {
     private static int angleEnDegre = 60;
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
+
+    @FXML
+    private Pane radar;
+    @FXML
+    private TextField comp1;
+    @FXML
+    private TextField comp2;
+    @FXML
+    private TextField comp3;
+    @FXML
+    private TextField comp4;
+    @FXML
+    private TextField comp5;
+    @FXML
+    private TextField comp6;
+    @FXML
+    private Button tracer;
+    @FXML
+    private Button vider;
+
 
 
     @Override
@@ -46,4 +67,21 @@ public class ToileController implements Initializable {
                 *  (value / noteMaximale));
     }
 
-}
+    @FXML
+    public void tracerPoints(){
+        ArrayList<Double> notes= new ArrayList<>();
+        notes.add(Double.valueOf(comp1.getText()));
+        notes.add(Double.valueOf(comp2.getText()));
+        notes.add(Double.valueOf(comp3.getText()));
+        notes.add(Double.valueOf(comp4.getText()));
+        notes.add(Double.valueOf(comp5.getText()));
+        notes.add(Double.valueOf(comp6.getText()));
+
+        Circle point = new Circle();
+        point.setCenterX(getXRadarChart(notes.get(0), 1));
+        point.setCenterY(getXRadarChart(notes.get(0), 5));
+        point.setRadius(5);
+        radar.getChildren().add(point);
+
+        }
+    }
